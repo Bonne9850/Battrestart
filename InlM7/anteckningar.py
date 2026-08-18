3efbebe8c6d74ff4af82a754b17cb8d85712b1b0
Rasmus Carnehult <rasmus.carnehult@gmail.com>
2026-08-18 20:19:50 +0200

    anteckningar mm

---
+++ InlM7/anteckningar.py
@@ -0,0 +1,20 @@
+# Skansholm s. 147 Anteckningar Kap 8 Skansholm
+
+# 8.1
+
+def circle_area(r):
+    return 3.14*r**2
+
+def circle_circumference(r):
+    return 2*3.14*r
+
+radius = 4
+
+print(circle_area(radius), circle_circumference(radius))
+
+# 8.2 s148 Skansholm
+
+def circle_area_and_circumference(r):
+    return 3.14*r**2, 2*3.14*r
+
+print(circle_area_and_circumference(radius))
